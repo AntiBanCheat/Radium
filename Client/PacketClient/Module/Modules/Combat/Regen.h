b48b05a70f2bcceb16e75783bc072a9fec0bd297
UauaYeah <111626537+UauaYeah@users.noreply.github.com>
2023-01-11 17:34:31 +0900

    Update Regen.h

--- Client/PacketClient/Module/Modules/Combat/Regen.h
+++ Client/PacketClient/Module/Modules/Combat/Regen.h
@@ -8,33 +8,35 @@ private:
     bool selectPickaxe();
     float animYaw = 0.f;
     vec3_ti blockPos;
-    vec3_t tempblockPos;
 
     float range = 4.5f;
     int delay = 6;
-    float animYawB;
-    float animPitchB;
-    bool smooth = false;
 
     int tick = 0;
     int slot = 0;
-    bool rot = false;
-    bool bypassrot = false;
-    bool firstbreak = false;
-    bool continuemine = false;
-    int rottime = 0;
-    vec3_t OldBlockPos;
 public:
-    int enabledticks;
     bool hasDestroyed = false;
     bool destroy = false;
     int blocksBroke = 0;
     bool swing = false;
+    bool bypassrot = false;
+    bool firstbreak = false;
+    bool continuemine = false;
+    float animYawB;
+    float animPitchB;
+    bool smooth = false;
+    bool rot = false;
+    int rottime = 0;
+    vec3_t OldBlockPos;
+    int rsTick = 0;
+    vec3_t tempblockPos;
     bool rotation = false;
     bool isregen = false;
     bool canattack = true;
+    int enabledticks;
 
     std::string name = "Regen";
+    std::string name2 = "";
     SettingEnum mode = this;
 
     virtual void onSendPacket(C_Packet* packet);
