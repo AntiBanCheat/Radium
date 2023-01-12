3702d7098bcbe2d32fcf1501c3441f427605187b
giyokun-9999 <65396222+giyokun-9999@users.noreply.github.com>
2023-01-12 19:39:25 +0900

    Update Blink.h

--- Client/PacketClient/Module/Modules/Player/Blink.h
+++ Client/PacketClient/Module/Modules/Player/Blink.h
@@ -6,9 +6,11 @@ class Blink : public IModule {
 private:
 	std::vector<PlayerAuthInputPacket*> PlayerAuthInputPacketHolder;
 	std::vector<C_MovePlayerPacket*> MovePlayerPacketHolder;
+	std::vector<C_PlayerActionPacket*> PlayerActionPacketHolder;
 public:
 	inline std::vector<PlayerAuthInputPacket*>* getPlayerAuthInputPacketHolder() { return &PlayerAuthInputPacketHolder; };
 	inline std::vector<C_MovePlayerPacket*>* getMovePlayerPacketHolder() { return &MovePlayerPacketHolder; };
+	inline std::vector<C_PlayerActionPacket*>* getPlayerActionPacketHolder() { return &PlayerActionPacketHolder; };
 	virtual void onTick(C_GameMode* gm);
 	virtual const char* getModuleName();
 	Blink();
