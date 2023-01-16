da80420f682c688214ca55bc90d54d2211170d7a
giyokun-9999 <65396222+giyokun-9999@users.noreply.github.com>
2023-01-16 22:01:18 +0900

    Update ColorUtil.h

--- Client/Utils/ColorUtil.h
+++ Client/Utils/ColorUtil.h
@@ -7,4 +7,9 @@ public:
 	static MC_Color rainbowColor(float seconds, float saturation, float brightness, long index);
 	static MC_Color astolfoRainbow(int yOffset, int yTotal);
 	static MC_Color interfaceColor(int index);
+	static MC_Color waveColortwo(int red, int green, int blue, int red2, int green2, int blue2, long index);
+	static MC_Color RGBWavetwo(int red, int green, int blue, int red2, int green2, int blue2, long index);
+	static MC_Color rainbowColortwo(float seconds, float saturation, float brightness, long index);
+	static MC_Color astolfoRainbowtwo(int yOffset, int yTotal);
+	static MC_Color interfaceColortwo(int index);
 };
