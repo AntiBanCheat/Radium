5cdaeed1bd797d67b721894a53d1df148c5785e1
AntiBanCheat <94786995+AntiBanCheat@users.noreply.github.com>
2023-01-28 21:43:35 +0900

    Rise ClickGUI

--- Client/PacketClient/Menu/ClickGui.h
+++ Client/PacketClient/Menu/ClickGui.h
@@ -30,11 +30,38 @@ struct ClickWindow {
 	int yOffset = 0;
 };
 
+struct ClickModule2 {
+	bool isExtended = false;
+};
+
+struct ClickWindow2 {
+	ClickWindow2() {
+		pos.x = 0;
+		pos.y = 0;
+
+		size.x = 30;
+		size.y = 30;
+	}
+	vec2_t pos;
+	vec2_t size;
+	bool isExtended = true;
+	bool isInAnimation = false;
+	float animation = 0;
+	const char* name;
+	vec4_t selectableSurface = vec4_t(pos.x, pos.y, pos.x + size.x, pos.y + size.y);
+
+	std::map<unsigned int, std::shared_ptr<ClickModule2>> moduleMap2;
+
+	int yOffset = 0;
+};
+
 class ClickGui {
 private:
 	char categoryNameTest;
 	inline static std::shared_ptr<ClickWindow> getWindow(const char* id);
 	inline static std::shared_ptr<ClickModule> getClickModule(std::shared_ptr<ClickWindow> window, const char* id);
+	inline static std::shared_ptr<ClickWindow2> getWindow2(const char* id);
+	inline static std::shared_ptr<ClickModule2> getClickModule2(std::shared_ptr<ClickWindow2> window, const char* id);
 
 	static void renderLabel(const char* text);
 	static void renderTooltip(std::string* text);
@@ -46,6 +73,7 @@ private:
 	static void renderTenacityCategory(Category category, MC_Color categoryColor, MC_Color categoryColor2);
 	static void renderTANACategory(Category category, MC_Color categoryColor);
 	static void rendertenaCategory(Category category);
+	static void renderNewCategory(Category category);
 	inline static void getModuleListByCategory(Category category, std::vector<std::shared_ptr<IModule>>* modList);
 
 	inline static const char* catToName(Category cat) {
