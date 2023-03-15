58c2450a17980a8e3063546d4598b54b3fe02506
AntiBanCheat <94786995+AntiBanCheat@users.noreply.github.com>
2023-03-15 11:12:07 +0800

    fixed bugs, add dmgflyspeed

--- Client/PacketClient/Module/Modules/Movement/Speed.cpp
+++ Client/PacketClient/Module/Modules/Movement/Speed.cpp
@@ -9,7 +9,7 @@ int enabledTicks = 0;
 int flareonticks = 0;
 int strafeTime = 0;
 int hivegroundticks = 0;
-
+float effectiveValue = 0.f;
 using namespace std;
 Speed::Speed() : IModule(0, Category::MOVEMENT, "Increases your speed") {
 	registerEnumSetting("Mode", &mode, 0);
@@ -27,11 +27,14 @@ Speed::Speed() : IModule(0, Category::MOVEMENT, "Increases your speed") {
 	mode.addEntry("Halcyon", 10);
 	mode.addEntry("Flareon", 11);
 	mode.addEntry("DamageSafe", 12);
+	mode.addEntry("DmgFlySpeed", 13); //only beta
 	// Vanilla
 	registerFloatSetting("Height", &height, height, 0.000001f, 0.42f);
 	// All Settings
 	registerFloatSetting("Speed", &speed, speed, 0.2f, 2.f);
-	registerIntSetting("Timer", &timer, timer, 20, 35);
+	registerIntSetting("Timer", &timer, timer, 15, 35);
+	registerIntSetting("BypassTime", &bypasstime, bypasstime, 1, 20);
+	registerIntSetting("DamageSpeed", &damagespeed, damagespeed, 0, 10); //only beta
 	registerBoolSetting("NoSlabs", &noslabs, noslabs);
 	registerBoolSetting("DesnycBoost", &dboost, dboost);
 	registerBoolSetting("Rotate", &rotate, rotate);
@@ -61,6 +64,7 @@ void Speed::onEnable() {
 	hivegroundticks = 0;
 	needblink = false;
 	damageMotion = 0;
+	strafeTime = 50;
 
 	oldx = player->currentPos.x;
 	oldz = player->currentPos.z;
@@ -121,46 +125,45 @@ void Speed::onMove(C_MoveInputHandler* input) {
 
 	bool pressed = MoveUtil::keyPressed();
 	if (!pressed) MoveUtil::stop(false);
-	if(mode.getSelectedValue() != 11) player->setSprinting(true);
+	if (mode.getSelectedValue() != 11) player->setSprinting(true);
 
-	if (rotate) {
-		float yaw = player->yaw;
+	float yaw = player->yaw;
 
-		if (input->forward && input->backward) {
-		}
-		else if (input->forward && input->right && !input->left) {
-			yaw += 45.f;
-		}
-		else if (input->forward && input->left && !input->right) {
-			yaw -= 45.f;
-		}
-		else if (input->backward && input->right && !input->left) {
-			yaw += 135.f;
-		}
-		else if (input->backward && input->left && !input->right) {
-			yaw -= 135.f;
-		}
-		else if (input->forward) {
-		}
-		else if (input->backward) {
-			yaw += 180.f;
-		}
-		else if (input->right && !input->left) {
-			yaw += 90.f;
-		}
-		else if (input->left && !input->right) {
-			yaw -= 90.f;
-		}
-
-		if (animYaw > yaw) animYaw -= ((animYaw - yaw) / 6);
-		else if (animYaw < yaw) animYaw += ((yaw - animYaw) / 6);
+	if (input->forward && input->backward) {
 	}
+	else if (input->forward && input->right && !input->left) {
+		yaw += 45.f;
+	}
+	else if (input->forward && input->left && !input->right) {
+		yaw -= 45.f;
+	}
+	else if (input->backward && input->right && !input->left) {
+		yaw += 135.f;
+	}
+	else if (input->backward && input->left && !input->right) {
+		yaw -= 135.f;
+	}
+	else if (input->forward) {
+	}
+	else if (input->backward) {
+		yaw += 180.f;
+	}
+	else if (input->right && !input->left) {
+		yaw += 90.f;
+	}
+	else if (input->left && !input->right) {
+		yaw -= 90.f;
+	}
+
+	if (animYaw > yaw) animYaw -= ((animYaw - yaw) / 6);
+	else if (animYaw < yaw) animYaw += ((yaw - animYaw) / 6);
 
 	// Vanilla
 	if (mode.getSelectedValue() == 0) {
 		static bool useVelocity = false;
 		// eat my absctrionalie
-		if (height >= 0.385) { if (player->onGround && pressed) player->jumpFromGround(); useVelocity = false; } else useVelocity = true;
+		if (height >= 0.385) { if (player->onGround && pressed) player->jumpFromGround(); useVelocity = false; }
+		else useVelocity = true;
 		if (height <= 0.04 && !input->isJumping) { player->velocity.y += height; useVelocity = false; }
 
 		if (player->onGround && useVelocity && pressed && !input->isJumping) player->velocity.y = height;
@@ -296,7 +299,7 @@ void Speed::onMove(C_MoveInputHandler* input) {
 		else useVelocity = true;
 		if (height <= 0.04 && !input->isJumping) { player->jumpFromGround(); player->velocity.y += height; useVelocity = false; }
 
-		if(speedFriction > 0.25) speedFriction *= duration;
+		if (speedFriction > 0.25) speedFriction *= duration;
 		if (pressed) {
 			if (player->onGround) {
 				if (useVelocity && !input->isJumping) player->velocity.y = height;
@@ -367,7 +370,8 @@ void Speed::onMove(C_MoveInputHandler* input) {
 
 		if (flareonticks >= 10 && speedFriction >= 0.4) {
 			speedFriction = 0.2;
-		}else speedFriction *= duration;
+		}
+		else speedFriction *= duration;
 		if (pressed) {
 			if (player->onGround) {
 				if (useVelocity && !input->isJumping) player->velocity.y = height;
@@ -410,7 +414,8 @@ void Speed::onMove(C_MoveInputHandler* input) {
 			}
 			if (player->onGround) {
 				speedFriction = randomFloat(speedMin, speedMax);
-			} else {
+			}
+			else {
 				speedFriction = randomFloat(.33f, .45f);
 			}
 			MoveUtil::setSpeed(speedFriction);
@@ -421,20 +426,66 @@ void Speed::onMove(C_MoveInputHandler* input) {
 	if (mode.getSelectedValue() == 12) {
 		if (pressed) {
 			if (player->onGround) {
+				MoveUtil::setSpeed(speed);
 				player->jumpFromGround();
 			}
 
 			if (damageMotion != 0 && damageMotion >= 0.15f) {
-				setSpeed(damageMotion + speed * 0.1);
+				setSpeed(damageMotion + speed * (damagespeed / 10));
 				damageMotion = 0;
 				strafeTime = 0;
 			}
 
 			if (MoveUtil::isMoving()) {
-				if (strafeTime <= 13) {
+				if (strafeTime <= bypasstime) {
 					strafeTime++;
 					setSpeed(player->velocity.magnitudexz());
 				}
+			}
+		}
+	}
+	//DamageFly       only beta
+	if (mode.getSelectedValue() == 13) {
+		if (pressed) {
+			if (player->onGround) {
+				MoveUtil::setSpeed(speed);
+				player->jumpFromGround();
+			}
+
+			if (damageMotion != 0 && damageMotion >= 0.15f) {
+				setSpeed((damageMotion + speed) * damagespeed);
+				damageMotion = 0;
+				strafeTime = 0;
+			}
+
+			if (MoveUtil::isMoving()) {
+				if (strafeTime < bypasstime) {
+					strafeTime++;
+					g_Data.getClientInstance()->minecraft->setTimerSpeed(10.f);
+					MoveUtil::setSpeed((damageMotion + speed) * 2);
+
+					float calcYaw = (player->yaw + 90) * (PI / 180);
+					float c = cos(calcYaw);
+					float s = sin(calcYaw);
+
+					vec2_t moveVec2D = { input->forwardMovement, -input->sideMovement };
+					moveVec2D = { moveVec2D.x * c - moveVec2D.y * s, moveVec2D.x * s + moveVec2D.y * c };
+					vec3_t moveVec;
+					player->velocity.y = 0;
+				}
+				else
+				{
+					if (strafeTime == bypasstime + 1)
+					{
+						MoveUtil::setSpeed(0);
+						MoveUtil::fullStop(true);
+						player->velocity.x = 0;
+						player->velocity.y = 0;
+						player->velocity.z = 0;
+						g_Data.getClientInstance()->minecraft->setTimerSpeed(20.f);
+					}
+				}
+
 			}
 		}
 	}
