aebd9790a97f8834a37a49b5c795c0245ee95184
AntiBanCheat <94786995+AntiBanCheat@users.noreply.github.com>
2023-04-19 20:39:43 +0900

    Update Interface.h

--- Client/PacketClient/Module/Modules/Visual/Interface.h
+++ Client/PacketClient/Module/Modules/Visual/Interface.h
@@ -7,7 +7,7 @@ class Interface : public IModule {
 public:
 	std::string clientName = "Radium";
 	std::string releaseStr = "Release";
-	std::string versionStr = "Release Build 4";
+	std::string versionStr;
 
 	std::string& getClientName() { return clientName; };
 public:
