cc144e3c364e63fa9bc67c15ff3092a929217207
AntiBanCheat <94786995+AntiBanCheat@users.noreply.github.com>
2023-04-24 19:54:07 +0900

    Update AutoTool.h

--- Client/PacketClient/Module/Modules/Player/AutoTool.h
+++ Client/PacketClient/Module/Modules/Player/AutoTool.h
@@ -1,19 +1,19 @@
 #pragma once
-#include "../../ModuleManager.h"
-#include "../Module.h"
+
+#include "../ModuleManager.h"
+#include "Module.h"
 
 class AutoTool : public IModule {
 public:
-    std::string name = "AutoTool";
-    bool hasClicked = false;
-
-    // Inherited via IModule
-    //virtual void onMove(C_MoveInputHandler* input) override;
-    //virtual void onSendPacket(C_Packet* packet) override;
-    // virtual void onPlayerTick(C_Player* plr) override;
-    //virtual const char* getRawModuleName() override;
-    virtual const char* getModuleName() override;
-    virtual void onTick(C_GameMode* gm) override;
-    virtual void onEnable() override;
-    AutoTool();
+	std::string name = "AutoTool";
+	bool hasClicked = false;
+	int prevslot = 0;
+	bool weapon = false;
+	bool tools = true;
+	// Inherited via IModule
+	virtual const char* getModuleName() override;
+	virtual void onTick(C_GameMode* gm) override;
+	virtual void onEnable() override;
+	virtual void onAttack(C_Entity* attackedEnt) override;
+	AutoTool();
 };
