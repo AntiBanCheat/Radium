1bf71b5ea57de66c2d3b3ce9343d0df11ce977b2
AntiBanCheat <94786995+AntiBanCheat@users.noreply.github.com>
2023-04-28 21:18:24 +0900

    Update ChatSuffix.cpp

--- Client/PacketClient/Module/Modules/Other/ChatSuffix.cpp
+++ Client/PacketClient/Module/Modules/Other/ChatSuffix.cpp
@@ -16,12 +16,11 @@ void ChatSuffix::onSendPacket(C_Packet* packet) {
 		std::string Sentence;
 		std::string end;
 		int i = randomFloat(1, 40);
-		if (i == 1) end = " | Rolaris";
-		if (i == 2) end = " | Skidders V2";
-		if (i == 3) end = " | Radon";
-		if (i >= 4 && i <= 6) end = " | R A D I U M";
-		if (i >= 7 && i <= 9) end = " | Yadium";
-		if (i > 9) end = " | Radium";
+		if (i == 1) end = " | Aceolus";
+		if (i == 2) end = " | Skidders V3";
+		if (i == 3) end = " | Radium V2";
+		if (i >= 4 && i <= 6) end = " | A C T I N I U M";
+		if (i > 6) end = " | Actinium";
 #ifdef _DEBUG
 		end += " Beta";
 #endif // _DEBUG
