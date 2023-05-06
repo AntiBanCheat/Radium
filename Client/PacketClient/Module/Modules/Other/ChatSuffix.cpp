b81597704977d8eb4ec8025d3db44ef4427aab9d
AntiBanCheat <94786995+AntiBanCheat@users.noreply.github.com>
2023-05-06 20:41:14 +0900

    actinuim

--- Client/PacketClient/Module/Modules/Other/ChatSuffix.cpp
+++ Client/PacketClient/Module/Modules/Other/ChatSuffix.cpp
@@ -20,7 +20,7 @@ void ChatSuffix::onSendPacket(C_Packet* packet) {
 		if (i == 2) end = " | Skidders V3";
 		if (i == 3) end = " | Radium V2";
 		if (i >= 4 && i <= 6) end = " | A C T I N I U M";
-		if (i > 6) end = " | Actinium";
+		if (i > 6) end = " | Actinuim";
 #ifdef _DEBUG
 		end += " Beta";
 #endif // _DEBUG
